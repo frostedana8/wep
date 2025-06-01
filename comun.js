6fff86b5f6187e027540b8546abff9bd56fa98c6
Nana <dcmjope@gmail.com>
2025-06-01 23:38:24 +0200

    Commit en linux i guess

--- comun.js
+++ comun.js
@@ -7,3 +7,15 @@ window.onload = function(){
         document.querySelector("header").innerHTML = data;
     });
 }
+
+
+
+//Programado a las 5am. Joder, si es que es cuando más productiva soy.
+function cambiarTriangulo(seccion){
+    var titulo = document.getElementById("tituloDatos" + seccion)
+    if(titulo.innerHTML.slice(-1) == "▲"){
+        titulo.innerHTML = titulo.innerHTML.replace("▲", "▼")
+    }else if(titulo.innerHTML.slice(-1) == "▼"){
+        titulo.innerHTML = titulo.innerHTML.replace("▼", "▲")
+    }
+}
